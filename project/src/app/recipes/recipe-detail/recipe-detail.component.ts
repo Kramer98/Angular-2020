c31c07c8fab509ba00b1a1d54ff5b2a7adcf8d28
Ashish Akondi -X (aakondi - ENQUERO INC at Cisco) <aakondi@cisco.com>
2020-09-19 18:37:54 +0530

    starting observables

--- project/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ project/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,24 +1,39 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
 
-import { Recipe } from '../recipe.model';
-import { ShoppingService } from 'src/app/shopping-list/shopping-list.service';
-import { Ingredient } from 'src/app/shared/ingredient.model';
+import { Recipe } from "../recipe.model";
+import { ShoppingService } from "src/app/shopping-list/shopping-list.service";
+import { Ingredient } from "src/app/shared/ingredient.model";
+import { ActivatedRoute, Router } from "@angular/router";
+import { RecipeService } from "../recipe.service";
 
 @Component({
-  selector: 'app-recipe-detail',
-  templateUrl: './recipe-detail.component.html',
-  styleUrls: ['./recipe-detail.component.css']
+  selector: "app-recipe-detail",
+  templateUrl: "./recipe-detail.component.html",
+  styleUrls: ["./recipe-detail.component.css"],
 })
 export class RecipeDetailComponent implements OnInit {
-  @Input() recipe: Recipe;
+  recipe: Recipe;
+  id: number;
 
-  constructor(private shoppingService: ShoppingService) { }
+  constructor(
+    private shoppingService: ShoppingService,
+    private route: ActivatedRoute,
+    private recipeService: RecipeService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
+    this.route.params.subscribe((params) => {
+      this.id = +params["id"];
+      this.recipe = this.recipeService.getRecipe(this.id);
+    });
   }
 
   toShoppingList() {
-    this.shoppingService.addToShopping(this.recipe.ingredients)
+    this.shoppingService.addToShopping(this.recipe.ingredients);
   }
 
+  onEditRecipe() {
+    this.router.navigate(["edit"], { relativeTo: this.route });
+  }
 }
--- project/src/app/recipes/recipe.service.ts
+++ project/src/app/recipes/recipe.service.ts
@@ -1,27 +1,31 @@
-import { Recipe } from './recipe.model'
-import { EventEmitter } from '@angular/core';
-import { Ingredient } from '../shared/ingredient.model';
+import { Recipe } from "./recipe.model";
+import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 
 export class RecipeService {
-    recipeSelected = new EventEmitter<Recipe>();
+  recipeSelected = new EventEmitter<Recipe>();
 
+  private recipes: Recipe[] = [
+    new Recipe(
+      "A test recipe",
+      "Tasty Schnitzel",
+      "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=700%2C636",
+      [new Ingredient("Meat", 1), new Ingredient("French Fries", 20)]
+    ),
 
-    private recipes: Recipe[] = [
-        new Recipe('A test recipe',
-            'Tasty Schnitzel',
-            'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=700%2C636',
-            [
-                new Ingredient('Meat', 1),
-                new Ingredient('French Fries', 20)
-            ]),
+    new Recipe(
+      "Big Fat Burger",
+      "This is a test",
+      "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=700%2C636",
+      [new Ingredient("Buns", 2), new Ingredient("Meat", 2)]
+    ),
+  ];
 
-        new Recipe('Big Fat Burger', 'This is a test',
-            'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=700%2C636',
-            [new Ingredient('Buns', 2), new Ingredient('Meat', 2)])
-    ];
+  getRecipes() {
+    return [...this.recipes];
+  }
 
-    getRecipes() {
-        return [...this.recipes];
-    }
-
+  getRecipe(id: number) {
+    return this.recipes[id];
+  }
 }
